ed55001937a0f5e48528195bd161e225d83c541c
Elvin Ramirez <real8686@gmail.com>
2018-08-17 23:15:53 -0400

    upodate README

---
+++ program.py
@@ -0,0 +1,13 @@
+### REAL Estate Miner
+
+
+topic
+
+- generator xpression
+- str magic method
+- repr magic method
+- string parsing
+- list compehension
+- csv file format
+- coding for python and python3
+- data pipelines with generators
