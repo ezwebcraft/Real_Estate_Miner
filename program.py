c508273676bf5aaf4160ded4b3b9775e050268e1
Elvin Ramirez <real8686@gmail.com>
2018-08-21 21:26:18 -0400

    feat: add basic load data

--- program.py
+++ program.py
@@ -2,7 +2,11 @@ import os
 
 
 def main():
-    pass
+    print_header()
+    filename = get_data_file()
+    # print(filename)
+    data = load_file(filename)
+    print(data)
 
 
 def print_header():
@@ -11,9 +15,15 @@ def print_header():
     print('----------------------------------')
     print()
 
+
 def get_data_file():
     base_folder = os.path.dirname(__file__)
-    os.path.join(base_folder,'data','SacramentoRealEstateTransactions2008.csv')
+    return os.path.join(base_folder, 'data', 'SacramentoRealEstateTransactions2008.csv')
+
+
+def load_file(filename):
+    return []
+
 
 if __name__ == '__main__':
     main()
