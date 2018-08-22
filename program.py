d22e154059122870cf2cb10ab6bf5130a44af884
Elvin Ramirez <real8686@gmail.com>
2018-08-21 21:18:00 -0400

    add base full path

--- program.py
+++ program.py
@@ -1,9 +1,19 @@
+import os
 
 
 def main():
     pass
 
 
+def print_header():
+    print('----------------------------------')
+    print('      Real Estate Miner           ')
+    print('----------------------------------')
+    print()
+
+def get_data_file():
+    base_folder = os.path.dirname(__file__)
+    os.path.join(base_folder,'data','SacramentoRealEstateTransactions2008.csv')
 
 if __name__ == '__main__':
     main()
