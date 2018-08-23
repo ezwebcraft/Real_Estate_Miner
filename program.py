6908cea9ae6220091f78b86f123964cdc7ed7131
Elvin Ramirez <real8686@gmail.com>
2018-08-23 15:28:36 -0400

    read data with dict

--- program.py
+++ program.py
@@ -24,11 +24,16 @@ def get_data_file():
 def load_file(filename):
     # return []
     with open(filename, 'r', encoding='utf-8') as fin:
-        header = fin.readline().strip()
-        reader = csv.reader(fin, delimiter=',')
+        reader = csv.DictReader(fin)
         for row in reader:
-            print(type(row),row)
-
+            #print(type(row),row)
+            print("Bed count: {}".format(row['beds']))
+#
+        #header = fin.readline().strip()
+        #reader = csv.reader(fin, delimiter=',')
+        #for row in reader:
+            #print(type(row),row)
+#
 
 
 
