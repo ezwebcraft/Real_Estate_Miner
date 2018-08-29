664d4e369b6d45d49aab58cd180d829772c5d217
Elvin Ramirez <real8686@gmail.com>
2018-08-28 22:31:25 -0400

    fix data_type mod

--- data_types.py
+++ data_types.py
@@ -1,34 +1,30 @@
-
 class Purchase:
-    def __init__(self, city,
-                 zipcode, state, beds,
-                 baths, sq__ft, home_type,
-                 sale_date, price, latitude,
-                 longitude):
-
-        self.street = street
-        self.city = city
-        self.zipcode = zipcode
-        self.state = state
-        self.beds = beds
-        self.baths = baths
-        self.sq__ft
-        self.home_type = home_type
-        self.sale_date = sale_date
-        self.price = price
-        self.latitude = latitude
+    def __init__(
+            self, city, zipcode, state, beds,
+            baths, sq__ft, home_type, sale_date, price,
+            latitude, longitude):
         self.longitude = longitude
+        self.latitude = latitude
+        self.price = price
+        self.sale_date = sale_date
+        self.type = home_type
+        self.sq__ft = sq__ft
+        self.baths = baths
+        self.beds = beds
+        self.state = state
+        self.zip = zipcode
+        self.city = city
 
     def create_from_dict(lookup):
         return Purchase(
-            lookup[city],
-            lookup[zipcode],
-            lookup[state],
-            int(lookup[beds]),
-            int(lookup[baths]),
-            int(lookup[sq__ft]),
-            lookup[home_type],
-            lookup[sale_date],
-            float(lookup[price]),
-            float(lookup[latitude]),
-            float(lookup[longitude]))
+            lookup['city'],
+            lookup['zip'],
+            lookup['state'],
+            int(lookup['beds']),
+            int(lookup['baths']),
+            int(lookup['sq__ft']),
+            lookup['type'],
+            lookup['sale_date'],
+            float(lookup['price']),
+            float(lookup['latitude']),
+            float(lookup['longitude']))
