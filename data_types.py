c9769d29ad6d9c0eeefd98c3071dfda5d4bd22af
Elvin Ramirez <real8686@gmail.com>
2018-11-02 18:58:22 -0400

    black the data_types file

--- data_types.py
+++ data_types.py
@@ -1,8 +1,18 @@
 class Purchase:
     def __init__(
-            self, city, zipcode, state, beds,
-            baths, sq__ft, home_type, sale_date, price,
-            latitude, longitude):
+        self,
+        city,
+        zipcode,
+        state,
+        beds,
+        baths,
+        sq__ft,
+        home_type,
+        sale_date,
+        price,
+        latitude,
+        longitude,
+    ):
         self.longitude = longitude
         self.latitude = latitude
         self.price = price
@@ -18,14 +28,15 @@ class Purchase:
     @staticmethod
     def create_from_dict(lookup):
         return Purchase(
-            lookup['city'],
-            lookup['zip'],
-            lookup['state'],
-            int(lookup['beds']),
-            int(lookup['baths']),
-            int(lookup['sq__ft']),
-            lookup['type'],
-            lookup['sale_date'],
-            float(lookup['price']),
-            float(lookup['latitude']),
-            float(lookup['longitude']))
+            lookup["city"],
+            lookup["zip"],
+            lookup["state"],
+            int(lookup["beds"]),
+            int(lookup["baths"]),
+            int(lookup["sq__ft"]),
+            lookup["type"],
+            lookup["sale_date"],
+            float(lookup["price"]),
+            float(lookup["latitude"]),
+            float(lookup["longitude"]),
+        )
